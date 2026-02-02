1980a425b67a3568f4278f01778b41ca54ede0d5
Anurag Singh <anuragsingh2445@gmail.com>
2026-02-02 19:05:45 +0530

    added file and edit menus

--- ui.py
+++ ui.py
@@ -3,9 +3,28 @@ import wx
 class NotepadAI(wx.Frame):
     def __init__(self, parent, title):
         super(NotepadAI, self).__init__(parent, title=title, size=(1000, 700))
-        self.Center()
+        self.InitMenuBar()
         self.InitUI()
+        self.Center()
         self.Show()
+
+    def InitMenuBar(self):
+        menubar = wx.MenuBar()
+        file_menu = wx.Menu()
+        file_menu.Append(wx.ID_NEW, "&New\tCtrl+N")
+        file_menu.Append(wx.ID_OPEN, "&Open...\tCtrl+O")
+        file_menu.Append(wx.ID_SAVE, "&Save\tCtrl+S")
+        file_menu.AppendSeparator()
+        file_menu.Append(wx.ID_EXIT, "E&xit\tAlt+F4")
+        
+        edit_menu = wx.Menu()
+        edit_menu.Append(wx.ID_CUT, "Cu&t\tCtrl+X")
+        edit_menu.Append(wx.ID_COPY, "&Copy\tCtrl+C")
+        edit_menu.Append(wx.ID_PASTE, "&Paste\tCtrl+V")
+        
+        menubar.Append(file_menu, "&File")
+        menubar.Append(edit_menu, "&Edit")
+        self.SetMenuBar(menubar)
 
     def InitUI(self):
         panel = wx.Panel(self)
